e544df00e5f6f86a7b1739cbd7d74c4fe10dde23
R0F7 <wwwrafikhan075@gmail.com>
2024-04-25 16:54:47 +0600

    add header addCoffee route

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,6 +5,7 @@ const Header = () => {
         <div className="flex justify-center items-center border-b">
             <ul className="space-x-10 py-7">
                 <NavLink to='/'>Home</NavLink>
+                <NavLink to='/addCoffee'>Add Coffee</NavLink>
                 <NavLink to='/users'>Users</NavLink>
                 <NavLink to='/signup'>Sign Up</NavLink>
                 <NavLink to='/signin'>Sign In</NavLink>
